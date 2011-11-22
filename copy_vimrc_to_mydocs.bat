6b61d87071bc8eeb2c9559235dbe4bbd284b6e60
Nick Klauer <nicholasklauer@alliantenergy.com>
2011-11-22 08:48:44 -0600

    I use *nix too much to remember Windows command-line

--- copy_vimrc_to_mydocs.bat
+++ copy_vimrc_to_mydocs.bat
@@ -2,5 +2,5 @@
 REM the _vimrc file in my Windows ~/ directory will get overwritten alot of the 
 REM time even if you use Junctions or Hard Linking.  So, to keep this vimrc 
 REM fresh with the _vimrc, copy it over;
-cp .\vimrc ..\_vimrc
+copy .\vimrc ..\_vimrc
 ECHO done
