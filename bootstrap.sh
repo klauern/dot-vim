365f43c5c1cfda25020ec7d7749b6c5d81d417c1
Nick Klauer <klauer@gmail.com>
2017-07-03 14:43:10 -0500

    add a bootstrap.sh script for new installs

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Use this script to set up your environment for vim if you have never done this before.
+
+set -ue
+
+readonly PROGNAME=$(basename "$0")
+readonly PROGDIR=$(readlink -m "$(dirname "$0")")
+
+cd $PROGDIR
+mkdir -p tmp/{backup,swap,undo}
+
+rm ~/.vimrc # if it exists
+ln -rs vimrc ../.vimrc
